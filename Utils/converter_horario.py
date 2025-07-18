73a8875f4dac603d2cb3d35cc374d3eec9583b0e
Jonathan Sposito <jnnspo@gmail.com>
2025-07-18 18:21:14 -0300

    Adicionada função para retornar horário no formato ISO 8601 de acordo com a documentação do Clockify

--- Utils/converter_horario.py
+++ Utils/converter_horario.py
@@ -1,20 +1,26 @@
-from datetime import datetime 
+from datetime import datetime, timezone
 import pytz
-
 
 def convert_to_br(iso_utc_str):
     if not iso_utc_str:
         return '[sem horário]'
 
-    # Remove o "Z" e adiciona o offset UTC se necessário
+    # Trata o formato com 'Z'
     if iso_utc_str.endswith('Z'):
-        iso_utc_str = iso_utc_str.replace('Z', '+00:00')
+        iso_utc_str = iso_utc_str.replace('Z', '')
 
     utc_time = datetime.fromisoformat(iso_utc_str)
 
-    # Define os fusos horários
+    # Se não tem tzinfo, adiciona UTC
+    if utc_time.tzinfo is None:
+        utc_time = pytz.utc.localize(utc_time)
+
+    # Converte para horário de Brasília
     br_tz = pytz.timezone("America/Sao_Paulo")
-    utc_time = utc_time.astimezone(pytz.utc)
     br_time = utc_time.astimezone(br_tz)
 
     return br_time.strftime('%d/%m/%Y %H:%M:%S')
+
+
+def horario_agora_iso():
+    return datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z")
